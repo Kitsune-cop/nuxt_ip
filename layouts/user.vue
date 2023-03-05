3f83044cbc0fc3cc1c84e76d59cab552669c6ba4
markTeerasak <twichai4869@gmail.com>
2023-03-05 14:17:36 +0700

    adduser layout

--- layouts/user.vue
+++ layouts/user.vue
@@ -1,91 +1,51 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      class="px-3"
-      color="white"
-      flat
-      density="compact"
-    >
-      <v-avatar
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
+    <v-app id="inspire">
+        <v-app-bar app=flat>
+            <v-avatar :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'" size="32"></v-avatar>
 
-      <v-spacer></v-spacer>
+            <v-tabs centered class="ml-n9" color="grey darken-1">
+                <v-tab v-for="link in links" :key="link">
+                    {{ link }}
+                </v-tab>
+            </v-tabs>
 
-      <v-tabs
-        centered
-        color="grey-darken-2"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-      <v-spacer></v-spacer>
+            <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar>
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col cols="12" sm="2">
+                        <v-sheet rounded="lg" min-height="268">
+                            <!--  -->
+                        </v-sheet>
+                    </v-col>
 
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
+                    <v-col cols="12" sm="8">
+                        <v-sheet min-height="70vh" rounded="lg">
+                            <!--  -->
+                        </v-sheet>
+                    </v-col>
 
-    <v-main class="bg-grey-lighten-3">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+                    <v-col cols="12" sm="2">
+                        <v-sheet rounded="lg" min-height="268">
+                            <!--  -->
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
-
+  
 <script>
-  export default {
+export default {
     data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
+        links: [
+            'Dashboard',
+            'Messages',
+            'Profile',
+            'Updates',
+        ],
     }),
-  }
+}
 </script>
